9433f43e032f8f42b6b509cc7943c087f70bc90f
Rami Abdou <ra494@cornell.edu>
2019-11-27 00:29:39 -0500

    Updated git ignore.

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 /* NPM Installation Dependencies */
 const express = require('express');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const path = require('path');
+const cors = require('cors');
+
+require('dotenv').config({ path: path.join(__dirname + '/.env') });
 
 /* Server Initialization */
 const app = express();
@@ -10,9 +15,40 @@ const server = require('http').Server(app);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+let corsOption = {
+  credentials: true,
+  exposedHeaders: ['x-auth-token'],
+  methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+  origin: true
+};
+
+app.use(cors(corsOption));
+
 app.route("/")
-  .get((request, response) => {
-    response.send("HELLO THERE!")
+  .post((request, response) => {
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_ACCOUNT,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+
+    let mailText = `Hello, I'm ${request.body.name} and I'm a ${request.body.position}
+      at ${request.body.company}. We are interested in sponsoring URMC! Please reach out to us
+      at ${request.body.email}. Cheers!`
+
+    const mailOptions = {
+      from: process.env.EMAIL_ACCOUNT,
+      to: 'ramiabdou98@gmail.com',
+      subject: `Interest in URMC Sponsorship from ${request.body.company}`,
+      text: mailText
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) response.send(error);
+      else response.send(info.response);
+    });
   });
 
 const port = process.env.PORT || 8080;
